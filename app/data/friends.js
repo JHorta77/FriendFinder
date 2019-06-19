7237a35c6f2e694903ed52838b27bc6c80a3f08e
Blackout077 <john@Johns-MBP.fios-router.home>
2019-06-19 16:08:15 -0400

    first change

---
+++ app/data/friends.js
@@ -0,0 +1,53 @@
+const friendsArry = [
+    {
+        name: "Jeremy Clarkson",
+        photo: "https://images.immediate.co.uk/production/volatile/sites/3/2018/05/Who-wants-to-be-a-millionaire-Jeremy-Clarkson-fbd5fd7.jpg?quality=90&resize=620,413",
+        score: [
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
+        ]
+    },
+    {
+        name: "Richard Hammond",
+        photo: "http://gmauthority.com/blog/wp-content/uploads/2018/09/Richard-Hammond-480x340.jpg",
+        score: [
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3,
+            3
+        ]
+    },
+    {
+        name: "James May",
+        photo: "https://upload.wikimedia.org/wikipedia/commons/a/ae/JamesMay.jpg",
+        score: [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
+        ]
+    },
+]
+
+
+module.exports = friendsArry;
