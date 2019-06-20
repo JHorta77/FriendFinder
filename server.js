68b63733ae8187b4e11bedcfebe5cebf6d87c53d
Blackout077 <john@Johns-MBP.fios-router.home>
2019-06-20 12:16:44 -0400

    2 change

--- server.js
+++ server.js
@@ -4,14 +4,14 @@ const bodyParser = require("body-parser");
 const path = require("path");
 const PORT = process.env.PORT || 8080;
 
-app.use(express.static(path.join(__dirname, './app/public')));
+app.use(express.static(path.join(__dirname, "/app/public")));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 
-require(path.join(__dirname, '/app/routing/apiRoutes'))(app);
-require(path.join(__dirname, '/app/routing/htmlRoutes'))(app);
+require(path.join(__dirname, "/app/routing/apiRoutes"))(app);
+require(path.join(__dirname, "/app/routing/htmlRoutes"))(app);
 
 app.listen(PORT, function () {
     console.log('Friend Finder app is listening on PORT: ' + PORT);
